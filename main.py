02d6bc80d456612e381dea7b09389b4fcb234674
roshanipetkar <85443740+roshanipetkar@users.noreply.github.com>
2021-07-28 22:50:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -75,6 +75,18 @@ def run_Rossie():
     else:
         talk('Please say the command again.')
 
+   def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am Rossie Sir. Please tell me how may I help you")
 
 while True:
     run_Rossie()
